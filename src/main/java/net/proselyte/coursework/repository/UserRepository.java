dca25a6dc26a2da4b3165bcb7c6c1a1436e993b6
VLAD <alsheuski@outlook.com>
2023-05-02 14:26:51 +0300

    removed all garbage

--- src/main/java/net/proselyte/coursework/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package net.proselyte.coursework.repository;
-
-import net.proselyte.coursework.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
